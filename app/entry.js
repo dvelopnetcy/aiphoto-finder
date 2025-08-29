9e26a6873adb013f9f3499041512d237c14f6472
dvelopnetcy <dvelopnet@gmail.com>
2025-08-29 16:12:18 +0300

    update

---
+++ app/entry.js
@@ -0,0 +1,7 @@
+// app/entry.js
+// Πρέπει να τρέχουν πριν από Ο,ΤΙΔΗΠΟΤΕ άλλο
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+
+// αφήνουμε το Expo Router να κάνει boot
+import 'expo-router/entry';
